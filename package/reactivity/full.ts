9b264ec91d3f02628abe7d32067a4554860ba6b8
cpWhitecat <19803cp@gmail.com>
2022-09-17 22:34:33 +0800

    feature : 先粗略看书

--- package/reactivity/full.ts
+++ package/reactivity/full.ts
@@ -20,4 +20,13 @@ const fullObj : object = new Proxy(data,{
         bocket.forEach(fn=>fn())
         return true 
     },
+    
 })
+
+
+// 为了避免执行多余的副作用
+// https://github.com/cpWhitecat/Vue-core/blob/main/packages/reactivity/src/effect.ts
+
+//   为了解决不必要的依赖收集 ， vue专门搞了个属性 来存储与之相关的依赖
+
+// 为了不多次执行相同的副作用 利用set数据结构去重的功能 ， 配合promise 可实现去只要结果的
