a303d359d91e4638fdb58aec8f310cbb00c4f60e
cpWhitecat <19803cp@gmail.com>
2022-10-08 15:25:27 +0800

    simple watch was application need test

--- package/reactivity/watch.ts
+++ package/reactivity/watch.ts
@@ -1,6 +1,7 @@
 import { effect, Options } from "./full";
+type watchOptions = {immediate?:boolean , flush?:'pre' | 'post' | 'sync'}
 
-function watch(effect , call_back){
+function watch(effect , call_back:(...args:any[])=>any , option:watchOptions){
     let getter ;
     if (typeof effect === 'function'){
         getter = effect
@@ -8,13 +9,31 @@ function watch(effect , call_back){
         getter = ()=>traverse(effect)
     }
 
+    let newValue,oldValue;
 
-    effect(()=>getter,{
-        lazy:true,
-        scheduler(){
-            call_back()
-        }
+    const job = ()=>{
+        newValue = effectFn();
+        call_back(oldValue , newValue);
+        oldValue = newValue
+    }
+
+    const effectFn = effect(()=>getter,{  // effect function was trigger
+        lazy:true,  //因为这是懒执行  ， 所以 newValue 没有立刻又结果 ， 必须要手动调动
+        scheduler:()=>{
+            if(option.flush === 'post'){
+                const p = Promise.resolve();
+                p.then(job)
+            }else{
+                job()
+            }
+        }  //job and job() is diff? Yep , one is result , and other one is function
     })
+
+   if(option.immediate){
+        job()
+   }else{
+        oldValue = effectFn()  //这是第一次调用  得到的是旧值
+   }
 }
 
 function traverse(source , Cache = new Set()){
@@ -28,3 +47,7 @@ function traverse(source , Cache = new Set()){
 
     return source
 }
+
+// 一些总结
+// 我们通过promise 来控制执行时机
+// 刚开始对于watch新旧值的理解根本不透彻 ,有点楞， 所以需要我之后再好好理解其中的思想 
