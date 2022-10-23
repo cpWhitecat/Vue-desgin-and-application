d21651b2fb7740aea6f0fa561b0a87b0d94899bb
cpWhitecat <19803cp@gmail.com>
2022-10-23 22:27:49 +0800

    add handler Map

--- package/reactivity/collectHandler.ts
+++ package/reactivity/collectHandler.ts
@@ -1,5 +1,5 @@
 import { ITERATE_KEY, track, trigger } from "./effect"
-import { toRaw } from "./reactive"
+import { toRaw ,reactive } from "./reactive"
 
 // 下面是个例子，待会封装到函数中\
 type Target = {
@@ -8,8 +8,12 @@ type Target = {
 
 type collectionTypes = Map<any,unknown> | Set<unknown> | WeakMap<any,any> | WeakSet<any>
 type SetType = Set<any>
+type MapType = Map<any,any>
 
-const mutableInstrumentations = {}
+const mutableInstrumentations = {
+    'add':add,
+    'delete':deleteEntry,
+}
 function add(this:SetType , p : unknown){
     const target = toRaw(this)
     const hadKey = target.has(p)
@@ -20,11 +24,33 @@ function add(this:SetType , p : unknown){
     return res
 }
 
+function deleteEntry(this:SetType,p : unknown){
+    const target = toRaw(this)
+    const hadKey = target.has(p);
+    const res = target.delete(p);
+    if(hadKey){
+        trigger(target,p,'DELETE')
+    }
 
-// this is test
-const setInstance = new Set()
+    return res
+}
 
-const collectHandler = new Proxy(setInstance,{
+
+function get(this:MapType,p:any){  // 那类型又如何处理呢
+    const target = toRaw(this);
+    const had = target.has(p);
+    if(had){
+        track(target,p)
+        const res = target.get(p)
+        return typeof res === 'object' ? reactive(res) : res
+    }
+
+
+    console.log(`have not key for ${target}`)
+}
+
+
+const collectHandler = {
     get(target, p, receiver) {
         if(p === 'raw') return target  // or receiver[raw]
         if(p === 'size'){
@@ -32,6 +58,6 @@ const collectHandler = new Proxy(setInstance,{
             return Reflect.get(target,p,target)
         }
 
-        return target[p].bind(target)
+        return mutableInstrumentations[p]
     },
-})
+}
