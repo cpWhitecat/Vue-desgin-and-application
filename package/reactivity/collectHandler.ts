7beb47fae268571771a1dfa91d96692061c323f4
cpWhitecat <19803cp@gmail.com>
2022-10-24 23:19:08 +0800

    create collection handler instance

--- package/reactivity/collectHandler.ts
+++ package/reactivity/collectHandler.ts
@@ -6,47 +6,45 @@ type Target = {
     'raw'?:any
 }
 
-type collectionTypes = Map<any,unknown> | Set<unknown> | WeakMap<any,any> | WeakSet<any>
-type SetType = Set<any>
-type MapType = Map<any,any>
+export type CollectionTypes = IterableCollections | WeakCollections
+
+type IterableCollections = Map<any, any> | Set<any>
+type WeakCollections = WeakMap<any, any> | WeakSet<any>
+type MapTypes = Map<any, any> | WeakMap<any, any>
+type SetTypes = Set<any> | WeakSet<any>
 
 const mutableInstrumentations = {
-    'add':add,
-    'delete':deleteEntry,
-}
-function add(this:SetType , p : unknown){
-    const target = toRaw(this)
-    const hadKey = target.has(p)
-    const res = target.add(p)
-    if(!hadKey){
-        trigger(target,p,"ADD")
+    add(this:SetTypes , p : unknown){
+        const target = toRaw(this)
+        const hadKey = target.has(p)
+        const res = target.add(p)
+        if(!hadKey){
+            trigger(target,p,"ADD")
+        }
+        return res
+    },
+    delete(this:CollectionTypes,p : unknown){
+        const target = toRaw(this)
+        const hadKey = target.has(p);
+        const res = target.delete(p);
+        if(hadKey){
+            trigger(target,p,'DELETE')
+        }
+    
+        return res
+    },
+    get(this:MapTypes,p:any){  // 那类型又如何处理呢
+        const target = toRaw(this);
+        const had = target.has(p);
+        if(had){
+            track(target,p)
+            const res = target.get(p)
+            return typeof res === 'object' ? reactive(res) : res
+        }
+    
+    
+        console.log(`not have key for ${target}`)
     }
-    return res
-}
-
-function deleteEntry(this:SetType,p : unknown){
-    const target = toRaw(this)
-    const hadKey = target.has(p);
-    const res = target.delete(p);
-    if(hadKey){
-        trigger(target,p,'DELETE')
-    }
-
-    return res
-}
-
-
-function get(this:MapType,p:any){  // 那类型又如何处理呢
-    const target = toRaw(this);
-    const had = target.has(p);
-    if(had){
-        track(target,p)
-        const res = target.get(p)
-        return typeof res === 'object' ? reactive(res) : res
-    }
-
-
-    console.log(`have not key for ${target}`)
 }
 
 
