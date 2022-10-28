c55f0b93aaf7fbdcf03037d6d41660ef052d09b1
cpWhitecat <19803cp@gmail.com>
2022-10-28 09:37:58 +0800

    simple ref is full but cannot use in HTMLELEMENT

--- package/reactivity/ref.ts
+++ package/reactivity/ref.ts
@@ -1,4 +1,10 @@
+import { track } from "./effect"
 import { reactive } from "./reactive"
+
+
+interface Ref {
+    isRef:true
+}
 
 export function ref(val:string | boolean | number | HTMLElement){
     const wrapper = {
@@ -8,4 +14,52 @@ export function ref(val:string | boolean | number | HTMLElement){
     Object.defineProperty(wrapper,'isRef',{value:true})
 
     return reactive(wrapper)
+}
+
+function toRef(obj,key){
+    return {
+        get value(){
+            return obj[key]
+        },
+        set value(val){
+            obj[key]= val
+        }
+    }
+}
+
+function toRefs(obj){
+    const result = {};
+
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            result[key] = toRef(obj,key);
+            
+        }
+    }
+
+    return result
+}
+
+
+function proxyRefs(ref:Ref){   //这里的ref was tracked
+    return new Proxy(ref,{
+        get(target, p, receiver) {
+            const refValue = Reflect.get(target,p,receiver);
+
+            return refValue.isRef ? refValue.value : refValue
+        },
+        set(target, p, newValue, receiver) {
+
+            const oldValue = target[p];
+
+            if(target.isRef){
+                oldValue.value = newValue
+                return true
+            }
+
+            return Reflect.set(target,p,newValue,receiver);
+
+            
+        },
+    })
 }
